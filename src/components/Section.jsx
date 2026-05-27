6797ab7c221b2d40c01838c83c9adaa8da68d774
Saikumar2323235 <mvvsaikumar23@gmail.com>
2026-05-27 13:00:16 +0530

    fix vercel build

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -8,7 +8,7 @@ function Section() {
             <p>Find Your Doctor for all your health problems</p>
         </div>
         <div>
-            <img src="https://chatgpt.com/backend-api/estuary/content?id=file_00000000df347209a806cb5ca3ada78a&ts=494406&p=fs&cid=1&sig=20f91897febe87e5c24b60061008cbb1e4801b5b1eac8200bf576c9230fff3a3&v=0" alt="" />
+            <img src={"/trumph.png"} alt="doctor" />
         </div>
     </section>
     /*
